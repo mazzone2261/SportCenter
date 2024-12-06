5ccf11bbcff373d372100db7eb0d16bd3b802fcc
Matteo Nello Zappa <matteo.zappa@itsrizzoli.it>
2024-12-06 10:37:29 +0100

    ciaociao

--- src/main/java/com/sportcenter/SportCenterBookingApplication.java
+++ src/main/java/com/sportcenter/SportCenterBookingApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SportCenterBookingApplication {
     public static void main(String[] args) {
         SpringApplication.run(SportCenterBookingApplication.class, args);
+    ciaociao
     }
 }
